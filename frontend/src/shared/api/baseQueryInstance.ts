283d6ff447d037250a5727769e6e56ae19c74413
mago <magomedovmarif2@gmail.com>
2025-01-24 03:38:54 +0300

    fix

--- frontend/src/shared/api/baseQueryInstance.ts
+++ frontend/src/shared/api/baseQueryInstance.ts
@@ -21,6 +21,8 @@ export class AxiosClient {
       withCredentials: true,
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        // "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS,HEAD", 
       },
     };
 
