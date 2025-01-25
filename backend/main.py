f481f08904666ecda6127a8a05b98382ade2d3d4
mago <magomedovmarif2@gmail.com>
2025-01-26 01:47:41 +0300

    delete decrypt

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Request
 from starlette.middleware.cors import CORSMiddleware
 
 from backend.database.connection.connection import DatabaseConnection
-from backend.middleware.decode_middleware import DecodeEncodeMiddleware
 from backend.routers import api_router
 
 
@@ -29,4 +28,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
-app.add_middleware(DecodeEncodeMiddleware)
