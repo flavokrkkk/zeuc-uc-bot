d6e30aaf3dd9a80ec714a82ab6bb009092260a6d
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2025-01-25 03:50:42 +0300

    ss

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -3,5 +3,6 @@ import Providers from "./providers/providers";
 // import { useActions } from "@/shared/hooks/useActions";
 
 export const App = () => {
+  console.log("object");
   return <Providers />;
 };
