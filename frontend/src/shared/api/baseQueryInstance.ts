12b6050501384ad20ef0f12bf43be491e4fbaf31
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2025-01-26 12:13:11 +0300

    small fix

--- frontend/src/shared/api/baseQueryInstance.ts
+++ frontend/src/shared/api/baseQueryInstance.ts
@@ -169,5 +169,6 @@ export class AxiosClient {
     }
   }
 }
+
 export const axiosNoAuth = new AxiosClient("https://zeusucbot.shop//api/");
 export const axiosAuth = new AxiosClient("https://zeusucbot.shop/api/", true);
