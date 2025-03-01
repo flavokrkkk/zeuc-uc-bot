9a6208c42ac6b545f0b9c609f4601698784aea72
mago <magomedovmarif2@gmail.com>
2025-03-02 01:12:26 +0300

    fix auth

--- bot/middleware/db_middleware.py
+++ bot/middleware/db_middleware.py
@@ -42,7 +42,7 @@ class DatabaseMiddleware(BaseMiddleware):
                     tg_id=user_data.id, 
                     username=user_data.username
                 )
-            if not user_in_db.username and not user_data.username:
+            if not user_in_db.username or not user_data.username:
                 length = randint(8, 10)
                 random_string = ''.join(choice(ascii_letters + digits) for _ in range(length))
                 await database.users.update_item(
